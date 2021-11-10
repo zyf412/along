039037a369cf200b38b09742dce4859bf4dbc72f
张雨凡 <281671126@qq.com>
2021-11-10 08:56:58 +0800

    添加进度条

--- src/main.js
+++ src/main.js
@@ -5,13 +5,19 @@ import App from './App.vue'
 import '@/assets/css/global.css'
 import router from './router'
 import axios from 'axios'
+import nProgress from 'nprogress'
 
 axios.defaults.baseURL = 'http://127.0.0.1:80/'
 Vue.config.productionTip = false
 axios.interceptors.request.use(config => {
   // 在request拦截器中，展示进度条nprogress.start()
-  // nProgress.start()
+  nProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+axios.interceptors.response.use(config => {
+  // 在response拦截器中，隐藏进度条nprogress.done()
+  nProgress.done()
   return config
 })
 Vue.prototype.$http = axios
